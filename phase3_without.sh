128adaa77a68abba4b44c9d378f9f34e556250d7
Seulgi Kim <dev@seulgi.kim>
2018-05-21 09:53:26 +0900

    Add script to run

---
+++ phase3_without.sh
@@ -0,0 +1,2 @@
+python ./prune_without_extension.py "data-without-extension-removed-$1" "data-without-extension-removed-pruned-$1" "$2"
+python ./usage_without_extension.py "data-without-extension-removed-pruned-$1"
